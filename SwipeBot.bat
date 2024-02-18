e92755918b18a86df45f4c028532d2e8427db3c5
tusharts <72263744+tusharts@users.noreply.github.com>
2024-02-19 03:40:57 +0530

    Update SwipeBot.bat

--- SwipeBot.bat
+++ SwipeBot.bat
@@ -1,11 +1,14 @@
 @echo off
 for /l %%i in (1, 0, 100) do (
     adb shell input touchscreen swipe 100 459 1260 472
+
+	 timeout /t 1 /nobreak
+
     adb shell input touchscreen swipe 100 459 1260 472
 
 	rem above command is to right swipe two times and can be changes as per requirements.
 
-    timeout /t 1 /nobreak
+  	timeout /t 1 /nobreak
 
 	rem this give break of 1 sec before changing the right swipe to left swipe
 
@@ -13,5 +16,6 @@ for /l %%i in (1, 0, 100) do (
 
 	rem above command is to left swipe one time and can be changes as per requirements.
 
+	timeout /t 1 /nobreak
 
 )
