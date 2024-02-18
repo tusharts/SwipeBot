c4e2cb30da1112e406f05e6d5a85cd7780909e56
tusharts <72263744+tusharts@users.noreply.github.com>
2024-02-19 01:56:04 +0530

    Update and rename AutoSwipe Automating Left & Right Swipes in Dating Apps.bat to SwipeBot.bat

--- AutoSwipe Automating Left & Right Swipes in Dating Apps.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for /l %%i in (1, 0, 100) do (
-    adb shell input touchscreen swipe 100 459 1260 472
-    adb shell input touchscreen swipe 100 459 1260 472
-    timeout /t 1 /nobreak
-    adb shell input touchscreen swipe 640 960 200 960
-)
---
+++ SwipeBot.bat
@@ -0,0 +1,17 @@
+@echo off
+for /l %%i in (1, 0, 100) do (
+    adb shell input touchscreen swipe 100 459 1260 472
+    adb shell input touchscreen swipe 100 459 1260 472
+
+	rem above command is to right swipe two times and can be changes as per requirements.
+
+    timeout /t 1 /nobreak
+
+	rem this give break of 1 sec before changing the right swipe to left swipe
+
+    adb shell input touchscreen swipe 640 960 200 960
+
+	rem above command is to left swipe one time and can be changes as per requirements.
+
+
+)
